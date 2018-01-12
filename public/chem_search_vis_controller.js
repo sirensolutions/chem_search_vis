dc96fc32660e02176f4cf6dbea3d04ecca39face
Miguel Pignatelli <emepyc@gmail.com>
2018-01-12 17:42:01 +0000

    improved vis

--- public/chem_search_vis_controller.js
+++ public/chem_search_vis_controller.js
@@ -4,14 +4,12 @@ import {FilterBarQueryFilterProvider} from 'ui/filter_bar/query_filter';
 
 
 const module = uiModules.get('kibana/chem_search_vis', ['kibana']);
-module.controller('ChemSearchVisController', function ($scope, Private, $http) {
+module.controller('ChemSearchVisController', function ($scope, Private, $http, $interpolate) {
   $scope.molSearchApiBaseUrl = 'https://smiles-to-fingerprint-dot-chemical-search.appspot.com';
 
   console.warn('chem search is loaded');
 
   const queryFilter = Private(FilterBarQueryFilterProvider);
-  console.log('all filters...');
-  console.log(queryFilter.getFilters());
 
   const moreLikeThisFilter = {
     meta: {
@@ -35,52 +33,31 @@ module.controller('ChemSearchVisController', function ($scope, Private, $http) {
   $scope.min = 50; // minimum stringency
   $scope.max = 100; // maximum stringency
 
+  updateVisibility(queryFilter.getFilters());
+
+  // update the searchSource when filters update
+  $scope.$listen(queryFilter, 'update', function () {
+    updateVisibility(queryFilter.getFilters());
+  });
+
   $scope.createFilter = function () {
 
     // here you have access to parameters saved
     // on the left hand side panel
     // when editing visualisation
     // e.g. you can grab "fields" and use them to generate the filter
-
     $http({
       url : `${$scope.molSearchApiBaseUrl}/fingerprint`,
       // url: 'https://localhost:8009/fingerprint',
       method: 'GET',
       params: {smiles: $scope.vis.params.smiles}
     }).success(function (response) {
-      console.log('fingerprint response...');
-      console.log(response);
-
-      // The following 2 statements are redundant??
-      $scope.vis.params.encoded_smiles = response;
       moreLikeThisFilter.query.more_like_this.like = response;
       moreLikeThisFilter.meta.alias = `Compound: ${$scope.vis.params.smiles}`;
       moreLikeThisFilter.meta.smiles = $scope.vis.params.smiles;
       moreLikeThisFilter.meta.source = 'mol-search';
       moreLikeThisFilter.query.more_like_this.fields = [$scope.vis.params.field];
       moreLikeThisFilter.query.more_like_this.minimum_should_match = $scope.stringency;
-
-      //
-      $scope.searchCompleted = true;
-
-      // pngDataToUrl(`${$scope.molSearchApiBaseUrl}/depict?smiles=${$scope.vis.params.smiles}`,
-      pngToDataUrl(`${$scope.molSearchApiBaseUrl}/depict?smiles=${$scope.vis.params.smiles}`,
-        function (base64Img) {
-          const img = document.getElementById('componentDiagram');
-          img.setAttribute('src', base64Img);
-        });
-
-      // $scope.currMolFilter = queryFilter.getFilters().filter((f) => f.meta.smiles);
-      // if ($scope.currMolFilter[0]) {
-      //   const currSmiles = $scope.currMolFilter[0].smiles;
-      //   if (currSmiles) {
-      //     console.log(`curr smiles to display in diagram: ${currSmiles}`);
-      //     pngDataToUrl(`${$scope.molSearchBaseUrl}/depict?smiles=${currSmiles}`, function (base64Img) {
-      //       const img = document.getElementById('componentDiagram');
-      //       img.setAttribute('src', base64Img);
-      //     });
-      //   }
-      // }
 
       queryFilter.addFilters(moreLikeThisFilter);
     });//TODO: handle errors
@@ -101,5 +78,19 @@ module.controller('ChemSearchVisController', function ($scope, Private, $http) {
       canvas = null;
     };
     img.src = url;
+  }
+
+  function updateVisibility(currFilters) {
+    // Set the visibility accordingly to the current filters...
+    // If there is a mol search filter currently applied or not
+    const molSearchFilter = currFilters.filter(f => f.meta && f.meta.smiles && !f.meta.disabled)[0];
+    if (molSearchFilter) {
+      pngToDataUrl(`${$scope.molSearchApiBaseUrl}/depict?smiles=${molSearchFilter.meta.smiles}`,
+        function (base64Img) {
+          const img = document.getElementById('componentDiagram');
+          img.setAttribute('src', base64Img);
+        });
+    }
+    $scope.searchCompleted = !!molSearchFilter;
   }
 });
